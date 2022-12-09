ba2fc9db41be4c4ffd301a6b8ea04536711fdce6
Acewill <williamaceprado@gmail.com>
2022-12-09 17:03:55 -0600

    Allowed changes

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://devearva-site.onrender.com'
+    'https://devearva-site.onrender.com',
+    'https://devearva-api.onrender.com'
 ];
 
 module.exports = allowedOrigins;
